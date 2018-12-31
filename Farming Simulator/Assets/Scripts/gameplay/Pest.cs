708552f9ae80a21aeb68e2ac4aea43c1c143e996
Arenz <aghecali@gmail.com>
2018-12-31 23:21:32 +0800

    Added pest and spawner
    added pest and spawner script
    -pests have health based on difficulty: 40hp for easy, 60hp for medium, 100hp for hard
    -spawner spawns pests based on difficulty and has different spawn rates based on difficulty, spawns 1 pest for easy at a rate of 80-90 seconds, 3 pests for medium at a rate of 48-72 seconds, 7 pests for hard at a rate of 24-36 seconds

---
+++ Farming Simulator/Assets/Scripts/gameplay/Pest.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * 
+ * Author: Antonio Lorenzo Hecali
+ * Date Created: 
+ * Source: 
+ * 
+ * Credits: -----------
+ * 
+ * License: 
+ * 
+ * Note:
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pest : MonoBehaviour
+{
+    public Transform targetPest;
+    private GameObject thepest;
+    float MoveSpeed = 0.2f;
+    float MinDist = 0f;
+    public GameObject[] plant;
+    public int plant_length;
+    private int randomIndex;
+    public int counter = 0;
+    public float pestsurvivaltimecounter;
+    public int pestsurvivaltimefinal;
+    public int pesthealth;
+    public GameObject targetplant;
+
+    void Start()
+    {
+        thepest = targetPest.gameObject;
+        if (Spawner.difficulty.Equals("easy"))
+        {
+            pesthealth = 40;
+        }
+        if (Spawner.difficulty.Equals("medium"))
+        {
+            pesthealth = 60;
+        }
+        if (Spawner.difficulty.Equals("hard"))
+        {
+            pesthealth = 100;
+        }
+    }
+
+    void Update()
+    {
+        if(pesthealth == 0)
+        {
+            Destroy(thepest);
+        }
+        MoveTowardsPlant();
+        pestsurvivaltimecounter += Time.deltaTime;
+    }
+
+    public int Compare(string x, string y)
+    {
+        return x.CompareTo(y);
+    }
+
+    void OnMouseDown()
+    {
+        //this will get the time the counter has when the pest was killed
+        pestsurvivaltimefinal = Mathf.RoundToInt(pestsurvivaltimecounter);
+        Spawner.pestlifespan = pestsurvivaltimefinal;
+        pesthealth -= 20;
+
+    }
+
+
+    public void MoveTowardsPlant()
+    {
+            //pass in the game object into your MoveToward() method
+            transform.LookAt(targetplant.transform);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            if (Vector3.Distance(transform.position, targetplant.transform.position) >= MinDist)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetplant.transform.position, MoveSpeed);
+            }
+    }
+}
---
+++ Farming Simulator/Assets/Scripts/gameplay/Spawner.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    private IList<GameObject> plants = new List<GameObject>();
+    public GameObject Pest;
+    public GameObject spawnedPest;
+    public static string difficulty = "easy";
+    private int numberspawn = 1;
+    public float timeleft = 90;
+    public static float pestlifespan;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	void Plant()
+    {
+
+    }
+	// Update is called once per frame
+	void Update () {
+        if(GameObject.FindGameObjectsWithTag("plant").Length != 0)
+        {
+            timeleft -= Time.deltaTime;
+        }
+        foreach (GameObject fooObj in GameObject.FindGameObjectsWithTag("plant"))
+        {
+
+            plants.Add(fooObj);
+        }
+        if((plants.Count > 0)&(timeleft < 0))
+        {
+            int randomIndex = Random.Range(0, plants.Count);
+
+            //pass in the game object into your MoveToward() method
+            Vector3 center = plants[randomIndex].transform.position;
+            if (difficulty.Equals("easy"))
+            {
+                numberspawn = 1;
+            }
+            if (difficulty.Equals("medium"))
+            {
+                numberspawn = 3;
+            }
+            if (difficulty.Equals("hard"))
+            {
+                numberspawn = 7;
+            }
+            for (int i = 0; i < numberspawn; i++)
+            {
+                Vector3 pos = RandomCircle(center, 20f);
+                Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center - pos);
+                spawnedPest = Instantiate(Pest, pos, Quaternion.identity);
+                spawnedPest.GetComponent<Pest>().targetplant = plants[randomIndex];
+            }
+        }
+        if(timeleft < 0)
+        {
+            if (difficulty.Equals("easy"))
+            {
+                timeleft = Random.Range(80, 90);
+            }
+            if (difficulty.Equals("medium"))
+            {
+                timeleft = Random.Range(80, 90);
+            }
+            if (difficulty.Equals("hard"))
+            {
+                timeleft = Random.Range(80, 90);
+            }
+        }
+    }
+    Vector3 RandomCircle(Vector3 center, float radius)
+    {
+        float ang = Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.z = center.z;
+        return pos;
+    }
+}
