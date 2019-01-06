08232348e68335f688363f58e616b02712483bfa
Gabriel <suarez.hanslee@gmail.com>
2019-01-07 00:14:47 +0800

    Initial playerprefs
    Playerprefs for tile identity (initial)

--- Farming Simulator/Assets/Scripts/gameplay/GameMgr.cs
+++ Farming Simulator/Assets/Scripts/gameplay/GameMgr.cs
@@ -51,38 +51,106 @@ public class GameMgr : MonoBehaviour {
         // Fill in details and instructions for help menu context
         //helptextContainer = GameObject.Find("helpmenucontext").GetComponent<Text>();
         helptextContainer.text = "HOW TO PLAY THE GAME";
-        
-        // Populate world space with tile sprites in an X x Y axis 
-        for (int x = 0; x < 40; x++)
-        {
-            initialX += (offsetX + 1);
 
-            for (int y = 1; y < 41; y++)
+        //if (PlayerPrefs.HasKey("grass(Clone)_000_001_posX"))
+        //{
+        //    Debug.Log("Loading existing save file data");
+
+        //    // Populate world space with tile sprites in an X x Y axis 
+        //    for (int x = 0; x < 40; x++)
+        //    {
+        //        initialX += (offsetX + 1);
+
+        //        for (int y = 1; y < 41; y++)
+        //        {
+        //            newY = initialY - (offsetY + (y - 1));
+
+        //            // If current tile is even
+        //            if (y % 2 == 0)
+        //            {
+        //                newX = initialX + 1f;
+
+        //                grass = Instantiate(objGrass, new Vector2(newX, newY), objGrass.rotation);
+        //                render = grass.GetComponent<SpriteRenderer>();
+
+        //                grass.name = grass.name + "_" + x.ToString("000") + "_" + y.ToString("000");
+
+        //            }
+
+        //            // If current tile is odd
+        //            else
+        //            {
+        //                grass = Instantiate(objGrass, new Vector2(initialX, newY), objGrass.rotation);
+        //                render = grass.GetComponent<SpriteRenderer>();
+
+        //                grass.name = grass.name + "_" + x.ToString("000") + "_" + y.ToString("000");
+        //            }
+        //        }
+        //    }
+        //}
+
+        //else
+        //{
+            // Populate world space with tile sprites in an X x Y axis 
+            for (int x = 0; x < 40; x++)
             {
-                newY = initialY - (offsetY + (y - 1));
+                initialX += (offsetX + 1);
 
-                // If current tile is even
-                if (y % 2 == 0)
+                for (int y = 1; y < 41; y++)
                 {
-                    newX = initialX + 1f;
+                    newY = initialY - (offsetY + (y - 1));
 
-                    grass = Instantiate(objGrass, new Vector2(newX, newY), objGrass.rotation);
-                    render = grass.GetComponent<SpriteRenderer>();
-                    //render.sortingOrder = y - 1;
+                    // If current tile is even
+                    if (y % 2 == 0)
+                    {
+                        newX = initialX + 1f;
 
-                    grass.name = grass.name + "_" + x.ToString("000") + "_" + y.ToString("000");
-                }
+                        grass = Instantiate(objGrass, new Vector2(newX, newY), objGrass.rotation);
+                        render = grass.GetComponent<SpriteRenderer>();
 
-                // If current tile is odd
-                else
-                {
-                    grass = Instantiate(objGrass, new Vector2(initialX, newY), objGrass.rotation);
-                    render = grass.GetComponent<SpriteRenderer>();
-                    //render.sortingOrder = y - 1;
+                        grass.name = grass.name + "_" + x.ToString("000") + "_" + y.ToString("000");
 
-                    grass.name = grass.name + "_" + x.ToString("000") + "_" + y.ToString("000");
+                        // Record position of the tile
+                        PlayerPrefs.SetFloat(grass.name + "_posX", this.gameObject.transform.position.x);
+                        PlayerPrefs.SetFloat(grass.name + "_posY", this.gameObject.transform.position.y);
+
+                        // Record tile definition
+                        PlayerPrefs.SetString(grass.name + "_Type", "");
+                        PlayerPrefs.SetInt(grass.name + "_isFarmable", 0);
+                        PlayerPrefs.SetInt(grass.name + "_isBuildable", 0);
+
+                        // Record things instantiated on the tiles
+                        PlayerPrefs.SetInt(grass.name + "_hasChild", 0);
+                        PlayerPrefs.SetString(grass.name + "_hasPlant", "");
+
+                    }
+
+                    // If current tile is odd
+                    else
+                    {
+                        grass = Instantiate(objGrass, new Vector2(initialX, newY), objGrass.rotation);
+                        render = grass.GetComponent<SpriteRenderer>();
+
+                        grass.name = grass.name + "_" + x.ToString("000") + "_" + y.ToString("000");
+
+                        // Record position of the tile
+                        PlayerPrefs.SetFloat(grass.name + "_posX", this.gameObject.transform.position.x);
+                        PlayerPrefs.SetFloat(grass.name + "_posY", this.gameObject.transform.position.y);
+
+                        // Record tile definition
+                        PlayerPrefs.SetString(grass.name + "_Type", "");
+                        PlayerPrefs.SetInt(grass.name + "_isFarmable", 0);
+                        PlayerPrefs.SetInt(grass.name + "_isBuildable", 0);
+
+                        // Record things instantiated on the tiles
+                        PlayerPrefs.SetInt(grass.name + "_hasChild", 0);
+                        PlayerPrefs.SetString(grass.name + "_hasPlant", "");
+
+                    }
                 }
             }
-        }
+        //}
+
+        //PlayerPrefs.DeleteAll();
     }
 }
